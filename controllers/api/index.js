5c137fb480e3673a73e63e3fbdd31ec09b98b575
AnnaC0rmier <anna.cormier.96@gmail.com>
2024-02-06 23:58:38 -0600

    added navbar to homeRoutes page

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
-const homeRoutes = require('./homeRoutes');
 const dashRoutes = require('./dashRoutes');
 const logInRoutes = require('./logInRoutes');
 
-router.use('/home', homeRoutes);
 router.use('/dash', dashRoutes);
 router.use('/log-in', logInRoutes);
 
