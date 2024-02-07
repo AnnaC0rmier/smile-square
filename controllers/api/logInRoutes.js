f3ebd4c41ece54c2585bc1b4699eaa022dfe85ff
AnnaC0rmier <anna.cormier.96@gmail.com>
2024-02-07 10:04:07 -0600

    connected all of my views to get requests

--- controllers/api/logInRoutes.js
+++ controllers/api/logInRoutes.js
@@ -1,9 +1,6 @@
 const router = require('express').Router();
 
-router.get('/login', async(req, res)=>{
-    res.render('logIn')
-   
-})
+
 
 
 module.exports = router
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,18 @@ const router = require('express').Router();
 
 router.get('/', async(req,res)=>{
     res.render('layouts/main')
+});
+
+
+router.get('/login', async(req, res)=>{
+    res.render('logIn')
+   
 })
+
+router.get('/dash', async(req, res)=>{
+    res.render('dash')
+   
+})
+
 
 module.exports = router;
