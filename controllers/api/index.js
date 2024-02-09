c9f0e0c883196b07b81e05d582db413ed4a93e03
AnnaC0rmier <anna.cormier.96@gmail.com>
2024-02-09 00:34:24 -0600

    created blog post script for client side

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const dashRoutes = require('./dashRoutes');
 const userRoutes = require('./userRoutes');
 
-router.use('/dashRoutes', dashRoutes);
+router.use('/dash', dashRoutes);
 router.use('/users', userRoutes);
 
 module.exports = router;
--- public/js/dash.js
+++ public/js/dash.js
@@ -1,1 +1,30 @@
-console.log('hellooooooo')
+const submitPost = async (event) => {
+    console.log('button clicked');
+    event.preventDefault();
+
+    const title = document.querySelector('#exampleFormControlInput1').value;
+    const content = document.querySelector('#exampleFormControlTextarea1').value;
+
+    if (content && title) {
+        try {
+            const response = await fetch('/api/dash/blogpost', {
+                method: 'POST',
+                body: JSON.stringify({ title, content }),
+                headers: { 'Content-Type': 'application/json' },
+            });
+
+            if (response.ok) {
+                document.location.replace('/');
+            } else {
+                alert('Something went wrong');
+            }
+        } catch (error) {
+            console.error('Error during fetch:', error);
+            alert('An error occurred while processing your request.');
+        }
+    } else {
+        alert('Please fill in both title and content fields.');
+    }
+};
+
+document.querySelector('.submit').addEventListener('click', submitPost);
